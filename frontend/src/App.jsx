d865e095bb90319d2c3a0d2962c8cafe5da8a180
Andrey Abramov <abramow-a.v@ya.ru>
2025-11-15 19:41:15 +0300

    add i18n

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col, Card, Button, Form, Tabs, Tab } from 'react-bootstrap'
 import { IconShieldLockFilled } from '@tabler/icons-react'
 
 const App = () => {
+  const { t, i18n } = useTranslation()
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-md-center">
@@ -11,35 +14,35 @@ const App = () => {
           <Card style={{ width: '18.4rem' }}>
             <Card.Body>
               <Card.Title className="text-center mb-3">
-                Secure Log
+                {t('company')}
                 <IconShieldLockFilled />
               </Card.Title>
               <Tabs defaultActiveKey="signIn" id="uncontrolled-tab" className="mb-3">
-                <Tab eventKey="signUp" title="Зарегистрироваться">
+                <Tab eventKey="signUp" title={t('signUp.tab')}>
                   <Form>
                     <Form.Group className="mb-3" controlId="signUpForm-username">
-                      <Form.Label>Заполните форму регистрации: </Form.Label>
-                      <Form.Control className="mb-3" type="name" placeholder="Имя Пользователя" />
+                      <Form.Label>{t('signUp.text')}</Form.Label>
+                      <Form.Control className="mb-3" type="name" placeholder={t('form.username')} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="signUpForm-email">
-                      <Form.Control className="mb-3" type="email" placeholder="Email" />
+                      <Form.Control className="mb-3" type="email" placeholder={t('form.email')} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="signUpForm-password">
-                      <Form.Control className="mb-3" type="password" placeholder="Пароль" />
+                      <Form.Control className="mb-3" type="password" placeholder={t('form.password')} />
                     </Form.Group>
-                    <Button className="w-100" variant="primary">Зарегистрироваться</Button>
+                    <Button className="w-100" variant="primary">{t('signUp.button')}</Button>
                   </Form>
                 </Tab>
-                <Tab eventKey="signIn" title="Войти">
+                <Tab eventKey="signIn" title={t('signIn.tab')}>
                   <Form>
                     <Form.Group className="mb-3" controlId="signInForm-username">
                       <Form.Label>Введите данные для входа: </Form.Label>
-                      <Form.Control className="mb-3" type="name" placeholder="Имя Пользователя" />
+                      <Form.Control className="mb-3" type="name" placeholder={t('form.username')} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="signInForm-password">
-                      <Form.Control className="mb-3" type="password" placeholder="Пароль" />
+                      <Form.Control className="mb-3" type="password" placeholder={t('form.password')} />
                     </Form.Group>
-                    <Button className="w-100" variant="primary">Войти</Button>
+                    <Button className="w-100" variant="primary">{t('signIn.button')}</Button>
                   </Form>
                 </Tab>
               </Tabs>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
